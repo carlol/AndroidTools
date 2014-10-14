b76aa44be31d2791fc427a515d0d179e6cc87e51
Carlo Luchessa <sc.game.podcast@gmail.com>
2014-10-14 21:38:58 +0200

    first steps

---
+++ js/main.js
@@ -0,0 +1,29 @@
+(function () {
+
+    // http://romannurik.github.io/AndroidAssetStudio/index.html
+
+    var tools = [
+        {
+            label: "Android Holo Colors Generator",
+            url: "http://android-holo-colors.com/"
+        }
+        , {
+            label: "Android Action Bar Style Generator",
+            url: "http://jgilfelt.github.io/android-actionbarstylegenerator/#name=example&compat=holo&theme=light&actionbarstyle=solid&texture=0&hairline=0&neutralPressed=1&backColor=E4E4E4%2C100&secondaryColor=D6D6D6%2C100&tabColor=33B5E5%2C100&tertiaryColor=F2F2F2%2C100&accentColor=33B5E5%2C100&cabBackColor=FFFFFF%2C100&cabHighlightColor=33B5E5%2C100"
+        }
+    ];
+
+    var $mainContent = $('#main_content');
+
+    var $menu = $('#menu_list');
+
+    $.each(tools, function( i, value ) {
+        var $item = $("<li>" + value.label + "</li>");
+        $item.click(function () {
+            $mainContent.attr('src', value.url);
+        });
+        $menu.append($item);
+    });
+
+
+}).apply(this);
