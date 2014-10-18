f3634a9eb790bc1f750b2249de6ac042e6130ef2
Carlo Luchessa <sc.game.podcast@gmail.com>
2014-10-18 15:21:34 +0200

    search filter added; other utilities added

--- js/main.js
+++ js/main.js
@@ -16,6 +16,14 @@
             url: "http://www.javascripter.net/faq/rgbtohex.htm"
         }
         , {
+            label: "Paletton",
+            url: "http://paletton.com/"
+        }
+        , {
+            label: "Google Design",
+            url: "http://www.google.com/design/spec/style/color.html"
+        }
+        , {
             label: "Json Formatter & Validator",
             url: "http://jsonformatter.curiousconcept.com/"
         }
@@ -31,6 +39,26 @@
             label: "IconsDB",
             url: "iconsdb.com"
         }*/
+        , {
+            label: "Notification Icon Generator",
+            url: "http://romannurik.github.io/AndroidAssetStudio/icons-notification.html"
+        }
+        , {
+            label: "Launcher Icon Generator",
+            url: "http://romannurik.github.io/AndroidAssetStudio/icons-launcher.html"
+        }
+        , {
+            label: "Action Bar and Tab Icon Generator",
+            url: "http://romannurik.github.io/AndroidAssetStudio/icons-actionbar.html"
+        }
+        , {
+            label: "Navigation Drawer Indicator Generator",
+            url: "http://romannurik.github.io/AndroidAssetStudio/icons-nav-drawer-indicator.html"
+        }
+        , {
+            label: "Simple Nine-patch Generator",
+            url: "http://romannurik.github.io/AndroidAssetStudio/nine-patches.html"
+        }
         , {
             label: "Android Holo Colors Generator",
             url: "http://android-holo-colors.com/"
@@ -49,6 +77,8 @@
 
     var $title = $('#title');
 
+    var $searchBox = $('#search');
+
     var $menu = $('#menu_list');
 
     var $basePage = $('.base-page');
@@ -60,31 +90,34 @@
         $redirectUrl.text("");
     });
 
-    $.each(tools, function (i, value) {
-        var $item = $("<li>" + value.label + "</li>");
-        $item.click(function () {
-            $basePage.css('display', 'none');
-            $mainContent.attr('data', value.url);
-            $menu.find('.' + activeClass).removeClass(activeClass);
-            $(this).addClass(activeClass);
-            $redirectUrl.text(value.url);
-            $redirectUrl.click(function () {
-                window.open(value.url, '_blank');
+    function searchFiltering() {
+        var searchFilter = $searchBox.val();
+        console.log(searchFilter);
+        $menu.children().remove();
+        $.each(tools, function (i, value) {
+            if (value.label.toLocaleUpperCase().indexOf(searchFilter.toLocaleUpperCase()) == -1) {
+                return; // skip
+            }
+            var $item = $("<li>" + value.label + "</li>");
+            $item.click(function () {
+                $basePage.css('display', 'none');
+                $mainContent.attr('data', value.url);
+                $menu.find('.' + activeClass).removeClass(activeClass);
+                $(this).addClass(activeClass);
+                $redirectUrl.text(value.url);
+                $redirectUrl.click(function () {
+                    window.open(value.url, '_blank');
+                });
             });
+            $menu.append($item);
         });
-        $menu.append($item);
-    });
+    }
 
-    /*$.each(tools, function (i, value) {
-        var $item = $("<li>" + value.label + "</li>");
-        $item.click(function () {
-            $bgImage.css('display', 'none');
-            $mainContent.attr('data', value.url);
-        });
-        $menu.append($item);
-    });*/
+    $searchBox.on('input', searchFiltering); // init
 
-    var initFn = function () {
+    // init
+    searchFiltering();
+    $(document).ready(function () {
         var windowHeight = $(window).height();
         $mainContent.attr('height', windowHeight);
         $headerContainer.css('height', windowHeight);
@@ -92,11 +125,6 @@
             //hoverThumbs: true,
             direction: 'vertical'
         });
-    };
-
-    // init
-    $(document).ready(initFn);
-
-    //$(document).resize(initFn);
+    });
 
 }).apply(this);
