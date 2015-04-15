bd1f28c0745a7233b1ab11f68d0f0fe6b2ea4bbd
Carlo Luchessa <carlo.luchessa@gmail.com>
2015-04-15 10:04:45 +0200

    links list added

--- js/main.js
+++ js/main.js
@@ -2,133 +2,126 @@
 
     var activeClass = 'tab-active';
 
-    var tools = [
-        /*{
-            label: "Google developer Console",
-            url: "https://console.developers.google.com/project"
-        }
-        ,*/
-        {
-            label: "DPI Calculator for Android",
-            url: "http://jennift.com/dpical.html"
+    var tools = {
+        widgets: [
+            {
+                label: "DPI Calculator for Android",
+                url: "http://jennift.com/dpical.html"
         }
         , {
-            label: "DP/PX converter",
-            url: "http://labs.rampinteractive.co.uk/android_dp_px_calculator/"
+                label: "DP/PX converter",
+                url: "http://labs.rampinteractive.co.uk/android_dp_px_calculator/"
         }
         , {
-            label: "Android Design Tips",
-            url: "http://www.androiddesign.tips/"
+                label: "Android Design Tips",
+                url: "http://www.androiddesign.tips/"
         }
          , {
-            label: "Android Design Cheatsheet",
-            url: "http://petrnohejl.github.io/Android-Cheatsheet-For-Graphic-Designers/"
+                label: "Android Design Cheatsheet",
+                url: "http://petrnohejl.github.io/Android-Cheatsheet-For-Graphic-Designers/"
         }
         , {
-            label: "Hex Color Tool",
-            url: "http://hexcolortool.com/"
+                label: "Hex Color Tool",
+                url: "http://hexcolortool.com/"
         }
         , {
-            label: "RGB-to-Hex Conversion",
-            url: "http://www.javascripter.net/faq/rgbtohex.htm"
+                label: "RGB-to-Hex Conversion",
+                url: "http://www.javascripter.net/faq/rgbtohex.htm"
         }
         , {
-            label: "Paletton",
-            url: "http://paletton.com/"
-        }
-//         , {
-//            label: "Material Palette",
-//            url: "http://www.materialpalette.com"
-//        }
-        , {
-            label: "Google Design",
-            url: "http://www.google.com/design/spec/style/color.html"
+                label: "Paletton",
+                url: "http://paletton.com/"
         }
         , {
-            label: "jsonschema2pojo",
-            url: "http://www.jsonschema2pojo.org/"
+                label: "Google Design",
+                url: "http://www.google.com/design/spec/style/color.html"
         }
         , {
-            label: "JsonLint",
-            url: "http://jsonlint.com/"
+                label: "jsonschema2pojo",
+                url: "http://www.jsonschema2pojo.org/"
         }
         , {
-            label: "Json Viewer",
-            url: "http://jsonviewer.stack.hu/"
-        }
-      /*  , {
-            label: "RegExr v2.0",
-            url: "http://www.regexr.com/"   
-        }*/
-        , {
-            label: "Android Arsenal",
-            url: "https://android-arsenal.com/"
-        }
-        /*, {
-            label: "IconsDB",
-            url: "iconsdb.com"
-        }*/
-        , {
-            label: "Android Layout Finder",
-            url: "https://www.buzzingandroid.com/tools/android-layout-finder/"
+                label: "JsonLint",
+                url: "http://jsonlint.com/"
         }
         , {
-            label: "TinyPNG",
-            url: "https://tinypng.com/"
-        }
-        /*, { // problem to load in <object>
-            label: "UI Prototype",
-            url: "http://www.b2tsoftware.com/developer/index.php"
-        }*/
-        /*, {
-            label: "Github - Material Design icons",
-            url: "https://github.com/google/material-design-icons"
-        }*/
-        , {
-            label: "Notification Icon Generator",
-            url: "http://romannurik.github.io/AndroidAssetStudio/icons-notification.html"
+                label: "Json Viewer",
+                url: "http://jsonviewer.stack.hu/"
         }
         , {
-            label: "Launcher Icon Generator",
-            url: "http://romannurik.github.io/AndroidAssetStudio/icons-launcher.html"
+                label: "Android Arsenal",
+                url: "https://android-arsenal.com/"
         }
         , {
-            label: "Action Bar and Tab Icon Generator",
-            url: "http://romannurik.github.io/AndroidAssetStudio/icons-actionbar.html"
+                label: "Android Layout Finder",
+                url: "https://www.buzzingandroid.com/tools/android-layout-finder/"
         }
         , {
-            label: "Android Button Maker",
-            url: "http://angrytools.com/android/button/"
-        }
-        /*, { // problem to load in <object>
-            label: "Navigation Drawer Indicator Generator",
-            url: "http://romannurik.github.io/AndroidAssetStudio/icons-nav-drawer-indicator.html"
-        }*/
-        , {
-            label: "Simple Nine-patch Generator",
-            url: "http://romannurik.github.io/AndroidAssetStudio/nine-patches.html"
+                label: "TinyPNG",
+                url: "https://tinypng.com/"
         }
         , {
-            label: "Android Holo Colors Generator",
-            url: "http://android-holo-colors.com/"
+                label: "Notification Icon Generator",
+                url: "http://romannurik.github.io/AndroidAssetStudio/icons-notification.html"
         }
         , {
-            label: "Android Action Bar Style Generator",
-            url: "http://jgilfelt.github.io/android-actionbarstylegenerator/"
+                label: "Launcher Icon Generator",
+                url: "http://romannurik.github.io/AndroidAssetStudio/icons-launcher.html"
         }
-        /*, {
-            label: "Roboto Font Families",
-            url: "https://gist.github.com/carlol/b2010d049b19acb157fb"
-        }*/
         , {
-            label: "Gradle, please",
-            url: "http://gradleplease.appspot.com/"
+                label: "Action Bar and Tab Icon Generator",
+                url: "http://romannurik.github.io/AndroidAssetStudio/icons-actionbar.html"
         }
-       /* , {
-            label: "Android Studio Tips",
-            url: "https://developer.android.com/sdk/installing/studio-tips.html"
-        }*/
-    ];
+        , {
+                label: "Android Button Maker",
+                url: "http://angrytools.com/android/button/"
+        }
+        , {
+                label: "Simple Nine-patch Generator",
+                url: "http://romannurik.github.io/AndroidAssetStudio/nine-patches.html"
+        }
+        , {
+                label: "Android Holo Colors Generator",
+                url: "http://android-holo-colors.com/"
+        }
+        , {
+                label: "Android Action Bar Style Generator",
+                url: "http://jgilfelt.github.io/android-actionbarstylegenerator/"
+        }
+        , {
+                label: "Gradle, please",
+                url: "http://gradleplease.appspot.com/"
+        }
+    ],
+        links: [
+            {
+                label: "Google developer Console",
+                url: "https://console.developers.google.com/project"
+        }, {
+                label: "Material Palette",
+                url: "http://www.materialpalette.com"
+        }, {
+                label: "RegExr v2.0",
+                url: "http://www.regexr.com/"
+        }, {
+                label: "IconsDB",
+                url: "http://www.iconsdb.com/"
+        }, {
+                label: "UI Prototype",
+                url: "http://www.b2tsoftware.com/developer/index.php"
+        }, {
+                label: "Github - Material Design icons",
+                url: "https://github.com/google/material-design-icons"
+        }, {
+                label: "Roboto Font Families",
+                url: "https://gist.github.com/carlol/b2010d049b19acb157fb"
+        }, {
+                label: "Android Studio Tips",
+                url: "https://developer.android.com/sdk/installing/studio-tips.html"
+        }
+    ]
+
+    };
 
     var $redirectUrl = $('#redirect_url');
 
@@ -140,42 +133,77 @@
 
     var $searchBox = $('#search');
 
-    var $menu = $('#menu_list');
+    var $menuWidget = $('#menu_widget_list');
+    var $menuLink = $('#menu_link_list');
 
     var $basePage = $('.base-page');
 
-    $title.click(function () {
+    // tabs
+    var $tabWidgets = $('.tab_widgets');
+    var $tabLinks = $('.tab_links');
+
+    function reset() {
         $basePage.css('display', 'block');
         $mainContent.attr('data', "");
-        $menu.find('.' + activeClass).removeClass(activeClass);
+        $menuWidget.find('.' + activeClass).removeClass(activeClass);
         $redirectUrl.text("");
-    });
+    }
 
-    function searchFiltering() {
-        var searchFilter = $searchBox.val();
-        console.log(searchFilter);
+    function selectMenuItem($menu, $item) {
+        $menu.find('.' + activeClass).removeClass(activeClass);
+        $item.addClass(activeClass);
+    }
+
+    function filterMenu($menu, itemList, searchFilter, isRedirect) {
         $menu.children().remove();
-        $.each(tools, function (i, value) {
+        $.each(itemList, function (i, value) {
             if (value.label.toLocaleUpperCase().indexOf(searchFilter.toLocaleUpperCase()) == -1) {
                 return; // skip
             }
-            var $item = $("<li>" + value.label + "</li>");
+            var $item = $('<li><span>' + value.label + '</span></li>');
+            if (isRedirect) {
+//                $item.append('<i class="uk-icon-hover uk-icon-link"></i>');
+            }
             $item.click(function () {
-                $basePage.css('display', 'none');
-                $mainContent.attr('data', value.url);
-                $menu.find('.' + activeClass).removeClass(activeClass);
-                $(this).addClass(activeClass);
-                $redirectUrl.text(value.url);
-                $redirectUrl.unbind( "click" );
-                $redirectUrl.click(function () {
+                if (isRedirect) {
                     window.open(value.url, '_blank');
-                });
+                } else {
+                    selectMenuItem($menu, $(this));
+                    $mainContent.attr('data', value.url);
+                    $basePage.css('display', 'none');
+                    $redirectUrl.text(value.url);
+                    $redirectUrl.unbind("click");
+                    $redirectUrl.click(function () {
+                        window.open(value.url, '_blank');
+                    });
+                }
             });
             $menu.append($item);
         });
     }
 
+    function searchFiltering() {
+        var searchFilter = $searchBox.val();
+        console.log(searchFilter);
+        filterMenu($menuWidget, tools.widgets, searchFilter);
+        filterMenu($menuLink, tools.links, searchFilter, true);
+    }
+
+    $title.click(function () {
+        reset();
+    });
+
     $searchBox.on('input', searchFiltering); // init
+
+    $tabWidgets.click(function () {
+        $menuLink.addClass('hidden');
+        $menuWidget.removeClass('hidden');
+    });
+    $tabLinks.click(function () {
+        reset();
+        $menuWidget.addClass('hidden');
+        $menuLink.removeClass('hidden');
+    });
 
     // init
     searchFiltering();
@@ -189,7 +217,7 @@
             //hoverThumbs: true,
             direction: 'vertical'
         });
-        $searchBox.width($headerContainer.width()-85);
+        $searchBox.width($headerContainer.width() - 85);
         $searchBox.attr('display', 'visible');
     };
 
